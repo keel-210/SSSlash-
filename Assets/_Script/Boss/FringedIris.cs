3056d1ae9cea12a61b9f56aeb7371b3dace103f3
keel-210 <crafters_touhou@yahoo.co.jp>
2019-06-21 02:15:23 +0900

    BossStageTemp

---
+++ Assets/_Script/Boss/FringedIris.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FringedIris : MonoBehaviour, IBoss, ISlide
+{
+    public List<Vector3> PosHistory { get; set; }
+    public void Slide(Vector2 p0, Vector2 p1, bool PlayerSide, Vector3 SlideVec)
+    {
+
+    }
+    public void Return(Vector2 p0, Vector2 p1, bool PlayerSide)
+    {
+
+    }
+
+    [SerializeField] public int Health { get; set; }
+    public void Damage()
+    {
+
+    }
+    public void Clear()
+    {
+
+    }
+}
