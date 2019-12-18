932b32afdde02bc77c2c6a83565268dbe7120eb2
keel-210 <crafters_touhou@yahoo.co.jp>
2019-12-19 02:03:03 +0900

    2D-base

---
+++ Assets/Standard Assets/Vehicles/Aircraft/Animation/PropPlane.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace AnimatorParameter
+{
+	[System.Serializable]
+	public class PropPlane
+	{
+		public Animator animator;
+
+		protected readonly static int GearStateHash = 195704262; public int GearState{ get{ return animator.GetInteger(GearStateHash); } set{ animator.SetInteger(GearStateHash, value); }}
+		public static readonly int Base_Layer_Raising = -2023849816;
+		public static readonly int Base_Layer_Lowering = 1706642163;
+		public static readonly int Base_Layer_Raised = 355052738;
+		public static readonly int Base_Layer_Lowered = 1251599115;
+
+	}
+}
